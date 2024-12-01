5adb7413316da4959346bfb280a367c16bfcc905
jasper.alkema@icloud.com <jasper.alkema@icloud.com>
2024-12-01 15:40:23 +0100

    Finished day 1!

--- src/Solutions/Day01.cs
+++ src/Solutions/Day01.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2024.Solutions
 {
@@ -6,12 +9,39 @@ namespace AdventOfCode2024.Solutions
     {
         public void RunPart1(string input)
         {
-            Console.WriteLine(input);
+            // First we get the input and parse it into two lists
+            (List<int> l, List<int> r) = ParseInput(input);
+
+            l.Sort();
+            r.Sort();
+
+            // For every index in the left list we calculate the difference in index between the two lists and sum them up to get the answer.
+            Console.WriteLine(l.Select((t, i) => Math.Abs(r[i] - t)).Sum());
         }
 
         public void RunPart2(string input)
         {
-            Console.WriteLine(input + " 2!");
+            // Again, we get the input into two lists
+            (List<int> l, List<int> r) = ParseInput(input);
+
+            // The similarity score is the sum of every item of l times the amount of times it (t) is in r
+            Console.WriteLine(l.Sum(t => t * r.Count(j => j == t)));
+        }
+
+        private static (List<int>, List<int>) ParseInput(string input)
+        {
+            // First we split the input into an array of strings
+            string[] splitInput = HelperFunctions.SplitLines(input);
+            List<int> l = [];
+            List<int> r = [];
+            foreach (string s in splitInput)
+            {
+                // Then for every line in the array we split it into two integers and add them to the lists
+                string[] ss = s.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                l.Add(int.Parse(ss[0]));
+                r.Add(int.Parse(ss[1]));
+            }
+            return (l, r);
         }
     }
 }
